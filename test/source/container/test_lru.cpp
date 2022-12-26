bc0f4ddbe4157670d8e311d37585f3f9681ffa30
peterlau123 <freelaupeter@gmail.com>
2022-12-26 13:41:08 +0800

    feat: In setup unit tests

---
+++ test/source/container/test_lru.cpp
@@ -0,0 +1,4 @@
+#include<lolly/container/LRU.hpp>
+
+
+
--- test/source/main.cpp
+++ test/source/main.cpp
@@ -1,3 +1,13 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
 #include <doctest/doctest.h>
+#include<gtest/gtest.h>
+
+//refer to http://google.github.io/googletest/primer.html
+//for more usage information
+int main(int argc,char**argv){
+    
+        ::testing::InitGoogleTest(&argc,argv);
+        
+        return RUN_ALL_TESTS();
+}
