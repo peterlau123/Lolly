7ce0dbe845786f94d5e69d7a01b79ee5c8a33a56
liuxin <liuxin@sensetime.com>
2023-01-05 23:34:48 +0800

    feat: add function wrapper

---
+++ include/lolly/Function.h
@@ -0,0 +1,15 @@
+#pragma once
+
+namespace Lolly {
+
+template <typename Sgnature> class Function;
+
+/*
+ *@class Function
+ *
+ *
+ *
+ * */
+template <typename F, typename Args..., typename R> class Function {};
+
+} // namespace Lolly
