cfc81a4745aa24de2b52b3c96ea8a22700fc72f1
peterlau123 <freelaupeter@gmail.com>
2023-02-20 10:17:23 +0800

    feat: add variant.hpp

---
+++ include/lolly/variant.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace Lolly{
+
+template <typename T>
+class VariantStorage{
+
+};
+
+template<typename ...Types>
+class VariantChoice{
+
+};
+
+
+
+template<typename ...Types>
+class MyVariant
+{
+
+};
+
+
+
+}
